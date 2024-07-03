f614f904d29017e469ce5bddafa0a0de94a54209
Alexander Nalobin <m_troc@mail.ru>
2024-07-03 05:42:57 +0300

    Added toy class

--- web/index.ts
+++ web/index.ts
@@ -1,12 +1,13 @@
 /**The idea of using this code exactly on typescript is to make the weakest
  *  point of validation of incoming json via schema.ts. All functional web
  *  code can be done in js without any hesitation.  */
-import { ButtplugMessageSchema } from "./schema";
+import * as bs from "./schema";
+import { Message } from "./schema";
 declare function logToConsole(param: string): void;
 
 export function handleMessage2(message: any, param?: any): string {
-  const schema = message as ButtplugMessageSchema;
-  console.log("", schema.LinearCmd?.DeviceIndex);
+  const schema = message as Message;
+  console.log("", schema);
 
   return "";
 }
