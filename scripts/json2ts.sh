70d453747a6880ac8ab777b4c6d4aa87c7037da1
Mihail Butvin <butvin.mihail@yandex.ru>
2024-07-12 03:53:26 +0300

    Change a license to WTFPL

--- scripts/json2ts.sh
+++ scripts/json2ts.sh
@@ -4,12 +4,6 @@
 schema_directory='spec/messages/v3/'
 ts_output_directory='web/components/schema'
 
-# Function for adding a license to the beginning of the file
-addLicensing() {
-    local file=$1
-    { cat license.txt; echo ; cat "$file"; } > "$file.tmp" && mv "$file.tmp" "$file"
-}
-
 # Function for generating TypeScript files from JSON schemas
 generateType() {
     local schema_file=$1
@@ -17,9 +11,7 @@ generateType() {
     echo "Generating ${output_file} from specification at ${schema_file}"
     ./node_modules/.bin/quicktype -s schema "$schema_file" -o "$output_file"  > /dev/null 2>&1
 
-    if [ -s "$output_file" ]; then
-        addLicensing "$output_file"
-    else
+    if [ ! -s "$output_file" ]; then
         rm -f "$output_file"
     fi
 }
