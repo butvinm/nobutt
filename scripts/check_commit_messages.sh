e0fa87cb01e493990714f28b0e7a2d080364e3a4
Mihail Butvin <butvin.mihail@yandex.ru>
2024-07-12 06:03:58 +0300

    14: Fix check commit message

--- scripts/check_commit_messages.sh
+++ scripts/check_commit_messages.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
-# Get base branch
+. scripts/toolz.sh
+
 base_branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
 
-echo "Base branch is $base_branch"
+lgbt_echo "Base branch is $base_branch"
 
-# Get the commit list
 commits=$(git log origin/$base_branch..HEAD --pretty=format:'%s')
 
-echo "Commits: $commits"
+lgbt_echo "Commits: $commits"
 
-# Check if there are any commits
 if [ -z "$commits" ]; then
-echo "No new commits found"
-exit 0
+    lgbt_echo "No new commits found"
+    exit 0
 fi
 
-# Check every commit
-echo "$commits" | while read commit; do
-if echo "$commit" | grep -qE "^Merge branch"; then
-    echo "Ignoring merge commit: $commit"
-    continue
-fi
+lgbt_echo "$commits"
+for commit in $commits; do
+    if lgbt_echo "$commit" | grep -qE "^Merge branch"; then
+        lgbt_echo "Ignoring merge commit: $commit"
+        continue
+    fi
 
-if ! echo "$commit" | grep -qE "^[0-9]+: .+$"; then
-    echo "Error: Commit message '$commit' does not match the required format '<issue number>: <commit message>'"
-    exit 1
-fi
+    if ! lgbt_echo "$commit" | grep -qE "^[0-9]+: .+$"; then
+        lgbt_echo "Error: Commit message '$commit' does not match the required format '<issue number>: <commit message>'"
+        exit 1
+    fi
 done
 
-echo "All commit messages are in the correct format."
+lgbt_echo "All commit messages are in the correct format."
